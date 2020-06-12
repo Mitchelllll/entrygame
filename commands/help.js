c7a2bb406cc636813c1b44a5a6ae90739b4c64de
Mitchell <mjbrinkhof17@gmail.com>
2020-06-12 13:40:48 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
 
         var text = `**${client.user.username} Commands** \n\n **__Commands__** \n `
         message.author.send(text);
-        message.channel.send('\`\`\`An help list has been send to your mailbox 📬\`\`\`');
+        message.channel.send(`\`\`\`An help list has been send to your mailbox (${message.author.diplayName})📬\`\`\``);
 
     } catch (error) {
         message.channel.send('\`\`\`🔴 An error has occurred.\`\`\`');

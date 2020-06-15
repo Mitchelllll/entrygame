4eab112d270a51609243854214dd710c1f1d095e
Mitchell <mjbrinkhof17@gmail.com>
2020-06-15 22:36:52 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -36,7 +36,6 @@ module.exports.run = async (client, message, args, prefix) => {
             createdChannel.send({
                 embed: {
                     title: `Hello ${message.author.username}`,
-
                     description: "Staff is on it's way, wait patiently.",
                 }
             });

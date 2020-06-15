226a79d3ee5302e192cc029dd13947307b270b8b
Mitchell <mjbrinkhof17@gmail.com>
2020-06-15 23:11:48 +0200

    Update close.js

--- commands/close.js
+++ commands/close.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args, prefix) => {
 
     if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("\`\`\`🔴 You do not have permission to close tickets.\`\`\`")
-    if(!message.channel.name.startsWith("ticket-")) {
+    if (message.channel.name.startsWith("ticket-")) {
         message.channel.delete();
     } else {
-        message.channel.send({embed: {
-            title: "This is not a ticket",
-            description: `The channel ${message.channel} is not a ticket.\n\n You can only use this command in tickets.`
-        }});
+        message.channel.send({
+            embed: {
+                title: "This is not a ticket",
+                description: `The channel ${message.channel} is not a ticket.\n\n You can only use this command in tickets.`
+            }
+        });
     }
 
 }

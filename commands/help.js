cd0a454d0effb3c00f15040a3e6a36b1e6d39372
Mitchell <mjbrinkhof17@gmail.com>
2020-06-16 00:54:00 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -35,7 +35,7 @@ module.exports.run = async (client, message, args, prefix) => {
         if (command["category"] == "Normal commands") {
             normal += `${prefix}${command["name"]} - ${prefix}${$command["aliases"]} - ${command["description"]}\n${prefix}${command["usage"]}\n\n`;
         } else if (command["category"] == "Admin commands") {
-            admin += `${prefix}${command["name"]} - ${prefix}${$command["aliases"]} - ${command["description"]}\n${prefix}${command["usage"]}\n\n`;
+            admin += `${prefix}${command["name"]} - ${prefix}${command["aliases"]} - ${command["description"]}\n${prefix}${command["usage"]}\n\n`;
         }
     }
 

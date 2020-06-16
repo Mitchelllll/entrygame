5b33d7d5ffdd123cb23863a558e4de2634280e5d
Mitchell <mjbrinkhof17@gmail.com>
2020-06-16 19:50:25 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args, prefix) => {
                 { name: "Server ID:", value: message.guild.id },
                 { name: "Server Owner:", value: message.guild.owner },
                 { name: "Server Region:", value: message.guild.region },
-                { name: "Server Created at:", value: message.guild.createdTimestamp - new Date() },
+                { name: "Server Created at:", value: new Date() - message.guild.createdTimestamp },
                 // { name: "Amount humans:", value: message.guild.id },
                 // { name: "Amount bots:", value: message.guild.id },
                 // { name: "Amount online:", value: message.guild.id },
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args, prefix) => {
                 // { name: "Voice channels:", value: message.guild.id },
 
                 { name: "Member count:", value: message.guild.memberCount },
-                { name: "You joined at:", value: message.guild.joinedTimestamp - new Date() },
+                { name: "You joined at:", value: new Date() - message.guild.joinedTimestamp },
             ],
             footer: {
                 text: message.member.displayName

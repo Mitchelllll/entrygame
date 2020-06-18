bdf057b4d04e5f2e5bc25d3e5a9b686edb4d798e
Mitchell <mjbrinkhof17@gmail.com>
2020-06-18 16:20:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const cooldowns = new Discord.Collection();
 
 const fs = require("fs");
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
+fs.readdir("./commands/", (error, files) => {
+    if (error) return console.log(error);
+    let fileCut = files.filter(file => file.split(".").pop() === "js");
+    if (fileCut.length <= 0) return console.log("No files to show");
+    fileCut.forEach((file) => {
+        let cmd = require(`./commands/${file}`);
+        let cmdName = file.split(".")[0];
+        console.log(`Loaded command: "${cmdName}".`);
+        client.commands.set(cmd.help.name, cmd);
+    });
+    console.log(" ");
+});
 
 client.on('guildMemberAdd', member => {
 
@@ -90,10 +96,37 @@ client.on("ready", () => {
 });
 
 client.on('message', async message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let command = args.shift().toLowerCase();
+    let commandFile = await client.commands.get(command);
+
+
+    if (commandFile) {
+        try {
+            if (message.channel.type === "dm") {
+                message.channel.send({
+                    embed: {
+                        title: `${findEmoji("Cross")} An error has occured.\nI'm not allowed to execute commands in DM channels yet.`,
+                        color: 0xff0000
+                    }
+                }).then(msg => msg.delete({ timeout: 5000 }));
+            }
+
+            commandFile.run(message, args);
+        } catch (err) {
+            console.log(err)
+        };
+    };
+
+
+
+
+
+    // if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    // const args = message.content.slice(prefix.length).split(/ +/);
+    // const commandName = args.shift().toLowerCase();
 
     var swearWords = JSON.parse(fs.readFileSync("./data/swearWords.json"));
 
@@ -108,10 +141,10 @@ client.on('message', async message => {
 
     }
 
-    // if (message.content.includes(client.user)) {
-    //     console.log("HI.")
-    //     // message.channel.send(`You woke me up! Do you need me?`);
-    // }
+    // // if (message.content.includes(client.user)) {
+    // //     console.log("HI.")
+    // //     // message.channel.send(`You woke me up! Do you need me?`);
+    // // }
 
     var prefixes = JSON.parse(fs.readFileSync("./data/botSettings.json"));
     if (!prefixes[message.guild.id]) {
@@ -122,77 +155,77 @@ client.on('message', async message => {
 
     var prefix = prefixes[message.guild.id].prefixes;
 
-    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-    if (!command) return;
+    // // const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    // // if (!command) return;
 
-    if (command.guildOnly && message.channel.type !== 'text') {
-        return message.channel.send({
-            embed: {
-                title: "Command not working",
-                description: "This command can not be used in DMs.",
-                color: "RED",
-                timestamp: new Date()
-            }
-        });
-    }
+    // // if (command.guildOnly && message.channel.type !== 'text') {
+    // //     return message.channel.send({
+    // //         embed: {
+    // //             title: "Command not working",
+    // //             description: "This command can not be used in DMs.",
+    // //             color: "RED",
+    // //             timestamp: new Date()
+    // //         }
+    // //     });
+    // // }
 
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`;
+    // // if (command.args && !args.length) {
+    // //     let reply = `You didn't provide any arguments, ${message.author}!`;
 
-        if (command.usage) {
-            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-        }
+    // //     if (command.usage) {
+    // //         reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    // //     }
 
-        return message.channel.send({
-            embed: {
-                title: "Proper usage",
-                description: reply,
-                color: "RED",
-                timestamp: new Date()
-            }
-        });
-    }
+    // //     return message.channel.send({
+    // //         embed: {
+    // //             title: "Proper usage",
+    // //             description: reply,
+    // //             color: "RED",
+    // //             timestamp: new Date()
+    // //         }
+    // //     });
+    // // }
 
-    if (!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
+    // // if (!cooldowns.has(command.name)) {
+    // //     cooldowns.set(command.name, new Discord.Collection());
+    // // }
 
-    const now = Date.now();
-    const timestamps = cooldowns.get(command.name);
-    const cooldownAmount = (command.cooldown || 3) * 1000;
+    // // const now = Date.now();
+    // // const timestamps = cooldowns.get(command.name);
+    // // const cooldownAmount = (command.cooldown || 3) * 1000;
 
-    if (timestamps.has(message.author.id)) {
-        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    // // if (timestamps.has(message.author.id)) {
+    // //     const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-        if (now < expirationTime) {
-            const timeLeft = (expirationTime - now) / 1000;
-            message.channel.send({
-                embed: {
-                    title: `Cooldown on ${command.name}`,
-                    description: `I'm sorry, you can use this command again in ${timeLeft.toFixed(1)} seconds.`,
-                    color: "GREEN",
-                    timestamp: new Date()
-                }
-            });
-        }
-    };
+    // //     if (now < expirationTime) {
+    // //         const timeLeft = (expirationTime - now) / 1000;
+    // //         message.channel.send({
+    // //             embed: {
+    // //                 title: `Cooldown on ${command.name}`,
+    // //                 description: `I'm sorry, you can use this command again in ${timeLeft.toFixed(1)} seconds.`,
+    // //                 color: "GREEN",
+    // //                 timestamp: new Date()
+    // //             }
+    // //         });
+    // //     }
+    // // };
 
-    timestamps.set(message.author.id, now);
-    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    // // timestamps.set(message.author.id, now);
+    // // setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.channel.send({
-            embed: {
-                title: "Error",
-                description: 'There was an error trying to execute that command!',
-                color: "RED",
-                timestamp: new Date()
-            }
-        });
-    }
+    // // try {
+    // //     command.execute(message, args);
+    // // } catch (error) {
+    // //     console.error(error);
+    // //     message.channel.send({
+    // //         embed: {
+    // //             title: "Error",
+    // //             description: 'There was an error trying to execute that command!',
+    // //             color: "RED",
+    // //             timestamp: new Date()
+    // //         }
+    // //     });
+    // // }
 });
 
 client.login(process.env.token);

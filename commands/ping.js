7842c904bdf781d894bd0dd993c592d5ba904bec
Mitchell <mjbrinkhof17@gmail.com>
2020-06-19 17:18:10 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
 module.exports.run = async (message, args) => {
 
     message.channel.send(`🏓 Pong!\nI reacted in ${parseInt(client.ws.ping)}ms!`);

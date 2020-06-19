1f5a68754ced24b335f25094a67dbd0215395837
Mitchell <mjbrinkhof17@gmail.com>
2020-06-19 16:20:57 +0200

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -7,18 +7,20 @@ module.exports.run = async (message, args) => {
     if (!msg) return message.channel.send({
         embed: {
             title: "Direct Message",
-            description: `You need to enter a message that you want to send to ${user}`,
+            description: `You need to enter a message that you want to send to ${user || user.name}`,
             color: "RED",
             timestamp: new Date(),
             footer: {
                 text: message.member.displayName
             }
         }
-    });
+    }).then(msg => msg.delete({ timeout: 5000 }));
+
     user.send(msg);
     message.channel.send({
         embed: {
-            title: `Succesfully send a message to ${user} (${user.id})`,
+            title: `Succesfully send a message to ${user || user.name} (${user.id})`,
+            description: "This message will be deleted in 5 seconds.",
             color: "GREEN",
             timestamp: new Date()
         }

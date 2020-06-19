21b23df2df6b243c05fd9ed3592698e717c07bc9
Mitchell <mjbrinkhof17@gmail.com>
2020-06-19 16:43:08 +0200

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js');
 module.exports.run = async (message, args) => {
 
-    var user = message.guild.member(message.mentions.users.first());
-    var userID = message.guild.members.cache.get(args[0]);
+    var user = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
     var msg = args.slice(1).join(" ");
 
     if (!msg) return message.channel.send({
         embed: {
             title: "Direct Message",
-            description: `You need to enter a message that you want to send to ${user || userID}`,
+            description: `You need to enter a message that you want to send to ${user}`,
             color: "RED",
             timestamp: new Date(),
             footer: {
@@ -17,21 +16,17 @@ module.exports.run = async (message, args) => {
         }
     }).then(msg => msg.delete({ timeout: 5000 }));
 
-    if (user == null) {
-        userID.send(msg)
-    } else {
-        user.send(msg);
-    }
+    user.send(msg);
 
     message.channel.send({
         embed: {
-            title: `Succesfully send a message to ${user || userID}`,
+            title: `Succesfully send a message to ${user}`,
             description: "This message will be deleted in 5 seconds.",
             color: "GREEN",
             timestamp: new Date()
         }
     }).then(msg => msg.delete({ timeout: 5000 }));
-    if (!message.author == user || userID) {
+    if (!message.author == user) {
         message.author.send({
             embed: {
                 title: "SUCCEEDED",

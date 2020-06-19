1017dee973bb3790e948e6dff2cebf08136b01fd
Mitchell <mjbrinkhof17@gmail.com>
2020-06-19 12:04:43 +0200

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,7 +1,10 @@
 const Discord = require('discord.js');
 module.exports.run = async (message, args) => {
 
+    var user = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0]);
+    var msg = args.slice(1).join(/ +/g);
 
+    message.user.send(msg);
 
 }
 

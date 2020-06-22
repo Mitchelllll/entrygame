9ca92763756bafc3a8e3d589fe2dedfe8e3ce917
Mitchell <mjbrinkhof17@gmail.com>
2020-06-22 12:21:05 +0200

    Update serverinfo.js

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -7,7 +7,12 @@ module.exports = {
     description: "Get information about this server!",
     guildOnly: true,
     run: async (message, args, emojis, prefix) => {
-        let time = new Date().toLocaleString("nl-NL", { timeZone: "Europe/Amsterdam" });
+
+        let date = message.member.joinedAt.split(" ");
+        let day = date.args[0];
+        let month = data.args[1];
+        let year = data.args[2];
+        let time = data.args[3];
 
         message.channel.send({
             embed: {
@@ -20,7 +25,7 @@ module.exports = {
                     { name: "Server ID:", value: message.guild.id },
                     { name: "Server Owner:", value: message.guild.owner },
                     { name: "Server Region:", value: message.guild.region },
-                    { name: "Server Created at:", value: message.guild.createdAt.toLocaleString('nl-NL', { timeZone: "Europe/Amsterdam" }) },
+                    { name: "Server Created at:", value: message.guild.createdAt },
                     // { name: "Amount humans:", value: message.guild.id },
                     // { name: "Amount bots:", value: message.guild.id },
                     // { name: "Amount online:", value: message.guild.id },
@@ -28,7 +33,7 @@ module.exports = {
                     // { name: "Voice channels:", value: message.guild.id },
 
                     { name: "Member count:", value: message.guild.memberCount },
-                    { name: "You joined at:", value: message.member.joinedAt.time },
+                    { name: "You joined at:", value: args[day, month, year, time] },
                 ],
                 footer: {
                     text: message.member.displayName

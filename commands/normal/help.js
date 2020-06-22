a2587dfbc494c5e71946c882aa969955ab875400
Mitchell <mjbrinkhof17@gmail.com>
2020-06-22 20:38:36 +0200

    Update help.js

--- commands/normal/help.js
+++ commands/normal/help.js
@@ -16,7 +16,8 @@ module.exports = {
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => `\n${command.name} - ${command.description}`)).replace(",", " ");
+            data.push(commands.map(command => `\n${command.name} - ${command.description}`));
+            data.replace(",", " ");
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
             return message.author.send(data, { split: true })

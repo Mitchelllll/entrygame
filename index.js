fc0a191296afd2f8f501ef12a5d0e14a89aac3dd
Mitchell <mjbrinkhof17@gmail.com>
2020-06-23 17:49:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const fs = require("fs");
 client.on("ready", () => {
     console.log(`${client.user.tag} is online!`);
 
-    let activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`]
+    let activities = [`${client.guilds.cache.size} servers!`, `${client.channels.cache.size} channels!`, `${client.users.cache.size} users!`], i = 0;
     setInterval(() => {
         client.user.setPresence({
             activity: {

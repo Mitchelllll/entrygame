460fc8018103a929afecbf6976fed0a87e57827b
Mitchell <mjbrinkhof17@gmail.com>
2020-06-23 16:25:14 +0200

    Update serverinfo.js

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -19,7 +19,7 @@ module.exports = {
                     { name: "Server ID:", value: message.guild.id },
                     { name: "Server Owner:", value: message.guild.owner },
                     { name: "Server Region:", value: message.guild.region },
-                    { name: "Server Created at:", value: message.guild.createdAt.toDateString('nl-NL') + " " + message.guild.createdAt.toTimeString('nl-NL', { timeZone: "Europe/Amsterdam" }) },
+                    { name: "Server Created at:", value: message.guild.createdAt.toDateString('en-GB') + " " + message.guild.createdAt.toTimeString('en-GB', { timeZone: "Europe/Amsterdam" }) },
                     // { name: "Amount humans:", value: message.guild.id },
                     // { name: "Amount bots:", value: message.guild.id },
                     // { name: "Amount online:", value: message.guild.id },
@@ -27,7 +27,7 @@ module.exports = {
                     // { name: "Voice channels:", value: message.guild.id },
 
                     { name: "Member count:", value: message.guild.memberCount },
-                    { name: "You joined at:", value: message.member.joinedAt },
+                    { name: "You joined at:", value: message.member.joinedAt.toDateString('en-GB') + " " + message.member.joinedAt.toTimeString('en-GB', { timeZone: "Europe/Amsterdam" }) },
                 ],
                 footer: {
                     text: message.member.displayName

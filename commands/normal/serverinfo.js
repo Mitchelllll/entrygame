9ea84d66a386a610986295b54a3c241b3037e7f8
Mitchell <mjbrinkhof17@gmail.com>
2020-06-23 16:58:56 +0200

    Update serverinfo.js

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -8,14 +8,6 @@ module.exports = {
     guildOnly: true,
     run: async (message, args, emojis, prefix) => {
 
-        let time = message.guild.createdAt.toTimeString("nl-NL", {timeZone: "Europe/Amsterdam"});
-        let date = message.guild.createdAt.toDateString("nl-NL", {timeZone: "Europe/Amsterdam"});
-        let str = `${time}: ${date}`
-
-        let time2 = message.member.joinedAt.toTimeString("nl-NL", {timeZone: "Europe/Amsterdam"});
-        let date2 = message.member.joinedAt.toDateString("nl-NL", {timeZone: "Europe/Amsterdam"});
-        let str2 = `${time2}: ${date2}`
-
         message.channel.send({
             embed: {
                 title: message.guild.name,
@@ -27,7 +19,7 @@ module.exports = {
                     { name: "Server ID:", value: message.guild.id },
                     { name: "Server Owner:", value: message.guild.owner },
                     { name: "Server Region:", value: message.guild.region },
-                    { name: "Server Created at:", value: str },
+                    { name: "Server Created at:", value: message.guild.createdAt.toLocaleDateString('nl_NL') + " " + message.guild.createdAt.toLocaleTimeString("nl_NL", { timeZone: "Europe/Amsterdam", hour12: false }) },
                     // { name: "Amount humans:", value: message.guild.id },
                     // { name: "Amount bots:", value: message.guild.id },
                     // { name: "Amount online:", value: message.guild.id },
@@ -35,7 +27,7 @@ module.exports = {
                     // { name: "Voice channels:", value: message.guild.id },
 
                     { name: "Member count:", value: message.guild.memberCount },
-                    { name: "You joined at:", value: str2 },
+                    { name: "You joined at:", value: message.member.joinedAt.toLocaleDateString('nl_NL') + " " + message.member.joinedAt.toLocaleTimeString("nl_NL", { timeZone: "Europe/Amsterdam", hour12: false }) },
                 ],
                 footer: {
                     text: message.member.displayName

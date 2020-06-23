57ed8ed2125a10e82fd8325e42f5c7e2b8b9a06d
Mitchell <mjbrinkhof17@gmail.com>
2020-06-23 16:56:21 +0200

    Update serverinfo.js

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -7,23 +7,14 @@ module.exports = {
     description: "Get information about this server!",
     guildOnly: true,
     run: async (message, args, emojis, prefix) => {
-        var date = message.guild.createdAt;
-        var search = /(.*)T(.*)\+/i;
-        var result = date.match(search);
 
-        var tempYear = result[1].split('-');
-        var tempTime = result[2].split(':');
+        let time = message.guild.createdAt.toTimeString("nl-NL", {timeZone: "Europe/Amsterdam"});
+        let date = message.guild.createdAt.toDateString("nl-NL", {timeZone: "Europe/Amsterdam"});
+        let str = `${time}: ${date}`
 
-        var dutchDate = tempYear[2] + ' ' + tempYear[1] + ' ' + tempYear[0] + ' - ' + tempTime[0] + ':' + tempTime[1];
-
-        var date2 = message.guild.createdAt;
-        var search = /(.*)T(.*)\+/i;
-        var result = date2.match(search);
-
-        var tempYear = result[1].split('-');
-        var tempTime = result[2].split(':');
-
-        var dutchDate2 = tempYear[2] + ' ' + tempYear[1] + ' ' + tempYear[0] + ' - ' + tempTime[0] + ':' + tempTime[1];
+        let time2 = message.member.joinedAt.toTimeString("nl-NL", {timeZone: "Europe/Amsterdam"});
+        let date2 = message.member.joinedAt.toDateString("nl-NL", {timeZone: "Europe/Amsterdam"});
+        let str2 = `${time2}: ${date2}`
 
         message.channel.send({
             embed: {
@@ -36,7 +27,7 @@ module.exports = {
                     { name: "Server ID:", value: message.guild.id },
                     { name: "Server Owner:", value: message.guild.owner },
                     { name: "Server Region:", value: message.guild.region },
-                    { name: "Server Created at:", value: dutchDate },
+                    { name: "Server Created at:", value: str },
                     // { name: "Amount humans:", value: message.guild.id },
                     // { name: "Amount bots:", value: message.guild.id },
                     // { name: "Amount online:", value: message.guild.id },
@@ -44,7 +35,7 @@ module.exports = {
                     // { name: "Voice channels:", value: message.guild.id },
 
                     { name: "Member count:", value: message.guild.memberCount },
-                    { name: "You joined at:", value: dutchDate2 },
+                    { name: "You joined at:", value: str2 },
                 ],
                 footer: {
                     text: message.member.displayName

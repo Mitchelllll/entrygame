5b927513b8258e188122b4efb603b497f08a68a7
Mitchell <mjbrinkhof17@gmail.com>
2020-06-23 16:37:56 +0200

    Update serverinfo.js

--- commands/normal/serverinfo.js
+++ commands/normal/serverinfo.js
@@ -19,7 +19,7 @@ module.exports = {
                     { name: "Server ID:", value: message.guild.id },
                     { name: "Server Owner:", value: message.guild.owner },
                     { name: "Server Region:", value: message.guild.region },
-                    { name: "Server Created at:", value: message.guild.createdAt.toDateString() + " " + message.guild.createdAt.toLocaleTimeString('en-GB') },
+                    { name: "Server Created at:", value: message.guild.createdAt.toDateString() + " " + message.guild.createdAt.toLocaleTimeString('nl-NL') },
                     // { name: "Amount humans:", value: message.guild.id },
                     // { name: "Amount bots:", value: message.guild.id },
                     // { name: "Amount online:", value: message.guild.id },
@@ -27,7 +27,7 @@ module.exports = {
                     // { name: "Voice channels:", value: message.guild.id },
 
                     { name: "Member count:", value: message.guild.memberCount },
-                    { name: "You joined at:", value: message.member.joinedAt.toDateString() + " " + message.member.joinedAt.toLocaleTimeString('en-GB') },
+                    { name: "You joined at:", value: message.member.joinedAt.toDateString() + " " + message.member.joinedAt.toLocaleTimeString('nl-NL') },
                 ],
                 footer: {
                     text: message.member.displayName
